c5f54638cebad761b34f3b16c1853ef47a7d0865
cgl <cgl@cgldeMacBook-Pro.local>
2026-01-15 23:25:33 +0800

    init

---
+++ download.sh
@@ -0,0 +1,9 @@
+#modelscope download --model="Qwen/Qwen2.5-0.5B-Instruct" --local_dir ./model-dir
+#mkdir Qwen
+#mv model-dir Qwen2.5-0.5B-Instruct
+#mv Qwen2.5-0.5B-Instruct Qwen
+
+modelscope download --model="Qwen/Qwen3-0.6B" --local_dir ./model-dir
+#mkdir Qwen
+mv model-dir Qwen3-0.6B
+mv Qwen3-0.6B Qwen
